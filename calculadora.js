61da8a6df224d7a60ddaa6399d21790602ed5aeb
daniapraezss <daniapraez@hotmail.com>
2021-07-09 14:00:39 -0500

    Calculadora

---
+++ calculadora.js
@@ -0,0 +1,14 @@
+let letra = document.getElementById("letra");
+let numero = document.getElementById("numero");
+let btnA1 = document.getElementById("btnA1");
+let btnB2 = document.getElementById("btnB2");
+let btnC3 = document.getElementById("btnC3");
+let btnD4 = document.getElementById("btnD4");
+let btnE5 = document.getElementById("btnE5");
+let btnF6 = document.getElementById("btnF6");
+let btnG7 = document.getElementById("btnG7");
+let btnH8 = document.getElementById("btnH8");
+let btnI9 = document.getElementById("btnI9");
+let btnJ0 = document.getElementById("btnJ0");
+
+//funciones
